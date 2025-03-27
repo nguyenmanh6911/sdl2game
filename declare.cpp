87bb732bb43b9d9aba19c07b4cdfa9aacaeb4790
nguyenmanh6911 <nmanh6911@gmail.com>
2025-03-27 14:19:00 +0700

    sửa interface

---
+++ declare.cpp
@@ -0,0 +1,66 @@
+#include "declare.h"
+
+int TOC_DO_DICH_CHUYEN_CUA_ONG=5;
+
+SDL_Window* window=NULL;
+SDL_Renderer* renderer=NULL;
+TTF_Font* font=NULL;
+TTF_Font* font1=NULL;
+Mix_Music* music=NULL;
+Mix_Music *music1=NULL;
+Mix_Chunk* effect1=NULL;
+Mix_Chunk *effect2=NULL;
+Mix_Chunk *effect3=NULL;
+SDL_Texture* backgroundTexture=NULL;
+SDL_Texture *birdTexture=NULL;
+SDL_Texture* birdTexture1=NULL;
+SDL_Texture *pipeTexture=NULL;
+SDL_Texture *backgroundTexture1=NULL;
+SDL_Texture* soundimage=NULL;
+SDL_Texture *muteimage=NULL;
+SDL_Texture *bombTexture=NULL;
+SDL_Texture* scoreTexture = NULL;
+SDL_Texture *highestscoreTexture = NULL;
+
+SDL_Color red = {255, 0, 0};
+SDL_Color white = {255, 255, 255};
+
+bool running = true;
+bool gamestarted = false;
+bool gameover = false;
+int bgPosition = 0;
+int frame = 0;
+int score = 0;
+int highestscore = 0;
+int van_toc=0;
+int gamemode=1;
+int winner=0;
+int van_toc1 = 0;
+int van_toc2 = 0;
+
+SDL_Rect bird1 = {100, SCREEN_HEIGHT / 2, CHIEU_RONG_KHUNG_HINH, CHIEU_DAI_KHUNG_HINH};
+SDL_Rect bird2 = {50, SCREEN_HEIGHT / 2, CHIEU_RONG_KHUNG_HINH, CHIEU_DAI_KHUNG_HINH};
+
+
+SDL_Rect bird;
+SDL_Rect flappybirdrect = {280, 50, 450, 150};
+SDL_Rect Startrect = {375, 250,250, 120};
+SDL_Rect instructionrect = {355, 400, 300, 120};
+SDL_Rect exit1rect = {410, 550, 175, 75};
+SDL_Rect textrect4 = {350, 50, 300, 100};
+SDL_Rect playagainrect = {350, 325, 325, 100};
+SDL_Rect exit2rect = {400, 500, 200, 75};
+SDL_Rect gameoverrect = {300,50,400,200};
+SDL_Rect soundrect = {25, 25, 60, 60};
+SDL_Rect scorerect = {675, 75, 50, 50};
+SDL_Rect mode1 = {225, 250,250, 120};
+SDL_Rect mode2 = {535, 250,250, 120};
+SDL_Rect highestscorerect = {675,150,50,50};
+
+string scoreText = "Score: " ;
+string highestscoreText = "Highest score: ";
+
+SDL_Rect animation[SO_KHUNG_HINH];
+SDL_Rect animation1[SO_KHUNG_HINH];
+
+vector <Pipe> pipes;
